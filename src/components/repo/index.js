66b25ca79a6fa95470e2ad0c05f167285f65ad98
Oleksandr Maksymenko <alexandr.maximenko@gmail.com>
2017-09-04 12:48:25 +0300

    Added cool rounding for stars

--- src/components/repo/index.js
+++ src/components/repo/index.js
@@ -15,6 +15,10 @@ export default class Repo extends Component {
         : '#586069'
     }
 
+    const stars = item.stargazers_count && item.stargazers_count < 1000
+      ? item.stargazers_count
+      : (item.stargazers_count / 1000).toFixed(1) + 'k'
+
     return(
       <div class={style.repo}
            onClick={this.handlerOnOpenDialog(item.id)}>
@@ -23,14 +27,14 @@ export default class Repo extends Component {
         {item.fork
           ? <span class={style.repoForked}>Forked</span>
           : null}
-        {item.stargazers_count
+        {stars
           ? <span class={style.repoStars}>
               <i class={style.repoStarsIcon}>
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 14 14" width="14px">
                   <path fillRule="evenodd" fill="#586069" d="M 14 6 l -4.9 -0.64 L 7 1 L 4.9 5.36 L 0 6 l 3.6 3.26 L 2.67 14 L 7 11.67 L 11.33 14 l -0.93 -4.74 Z"/>
                 </svg>
               </i>
-              {item.stargazers_count}
+              {stars}
             </span>
           : null}
         <time class={style.repoUpdatedAt} dateTime={item.updated_at}>Updated at: {item.updated_at.slice(0,10)}</time>
